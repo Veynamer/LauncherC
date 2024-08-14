ae217f5e497efec063401cdb355bf751e0e25f08
Veynamer <113140575+Veynamer@users.noreply.github.com>
2024-08-14 14:14:36 +0300

    Add files via upload

--- chair/src/app/lws/launcherc/BatteryWidget.kt
+++ chair/src/app/lws/launcherc/BatteryWidget.kt
@@ -1,10 +1,12 @@
 package app.lws.launcherc
 
+import android.Manifest
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
 import android.os.BatteryManager
 import android.widget.RemoteViews
 import com.android.launcher3.R
@@ -18,29 +20,37 @@ class BatteryWidget : AppWidgetProvider() {
     }
 
     private fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
-        val batteryStatus = context.registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
-        val level = batteryStatus?.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) ?: -1
-        val scale = batteryStatus?.getIntExtra(BatteryManager.EXTRA_SCALE, -1) ?: -1
-        val batteryPct = level * 100 / scale.toFloat()
+        val batteryLevel = getBatteryLevel(context)
 
         val views = RemoteViews(context.packageName, R.layout.battery_widget_layout)
-        views.setTextViewText(R.id.battery_percentage, "%.0f%%".format(batteryPct))
-        
+        views.setTextViewText(R.id.battery_percentage, "%.0f%%".format(batteryLevel))
+
         val batteryDrawableId = when {
-            batteryPct >= 100 -> R.drawable.battery_full
-            batteryPct >= 90 -> R.drawable.battery_90
-            batteryPct >= 80 -> R.drawable.battery_80
-            batteryPct >= 70 -> R.drawable.battery_70
-            batteryPct >= 60 -> R.drawable.battery_60
-            batteryPct >= 50 -> R.drawable.battery_50
-            batteryPct >= 40 -> R.drawable.battery_40
-            batteryPct >= 30 -> R.drawable.battery_30
-            batteryPct >= 20 -> R.drawable.battery_20
-            batteryPct >= 10 -> R.drawable.battery_10
+            batteryLevel >= 100 -> R.drawable.battery_full
+            batteryLevel >= 90 -> R.drawable.battery_90
+            batteryLevel >= 80 -> R.drawable.battery_80
+            batteryLevel >= 70 -> R.drawable.battery_70
+            batteryLevel >= 60 -> R.drawable.battery_60
+            batteryLevel >= 50 -> R.drawable.battery_50
+            batteryLevel >= 40 -> R.drawable.battery_40
+            batteryLevel >= 30 -> R.drawable.battery_30
+            batteryLevel >= 20 -> R.drawable.battery_20
+            batteryLevel >= 10 -> R.drawable.battery_10
             else -> R.drawable.battery_empty
         }
         views.setImageViewResource(R.id.battery_icon, batteryDrawableId)
-        
+
         appWidgetManager.updateAppWidget(appWidgetId, views)
+    }
+
+    private fun getBatteryLevel(context: Context): Float {
+        return if (context.checkSelfPermission(Manifest.permission.BATTERY_STATS) == PackageManager.PERMISSION_GRANTED) {
+            val batteryStatus = context.registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+            val level = batteryStatus?.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) ?: -1
+            val scale = batteryStatus?.getIntExtra(BatteryManager.EXTRA_SCALE, -1) ?: -1
+            level * 100 / scale.toFloat()
+        } else {
+            -1f
+        }
     }
 }
